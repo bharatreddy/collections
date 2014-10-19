ea28d21cd0dab759e8232e5709fce1e78856fde2
Bharat Kunduri <bharatr@vt.edu>
2014-10-18 20:47:44 -0700

    Bug Fix

--- cricDB.py
+++ cricDB.py
@@ -1,7 +1,8 @@
 if __name__ == "__main__":
     import cricDB
     dbo = cricDB.DbUtils()
-
+    dbo.create_main_tables()
+    dbo.close()
 
 class DbUtils(object):
     """
@@ -16,67 +17,76 @@ class DbUtils(object):
         self.cursor = self.conn.cursor()
 
     def create_main_tables(self):
+        import mysql.connector
         # create the Match table
         mtchStr = """
-                    CREATE TABLE Match (
-                        Id INT PRIMARY KEY NOT NULL,
-                        Date DATE NOT NULL,
+                    CREATE TABLE Game(
+                        Id INT NOT NULL,
+                        MatchDate DATETIME NOT NULL,
                         Type VARCHAR(50) NOT NULL,
                         Venue VARCHAR(50) NOT NULL,
                         Competition VARCHAR(50),
-                        Overs FLOAT
+                        Overs FLOAT,
+                        PRIMARY KEY (Id)
                         )
                     """
         self.cursor.execute(mtchStr)
         # create the Toss table
         tossStr = """
-                    CREATE TABLE Toss (
-                        MatchId INT ,
+                    CREATE TABLE Toss(
+                        MatchId INT NOT NULL,
                         Winner VARCHAR(100) NOT NULL,
                         Decision VARCHAR(50) NOT NULL,
-                        FOREIGN KEY (MatchId) REFERENCES Match(Id)
+                        FOREIGN KEY (MatchId) REFERENCES Game(Id)
                         )
                     """
         self.cursor.execute(tossStr)
         # create the Teams table
         teamsStr = """
-                    CREATE TABLE Teams (
-                        MatchId INT ,
+                    CREATE TABLE Teams(
+                        MatchId INT NOT NULL,
                         Team1 VARCHAR(100) NOT NULL,
                         Team2 VARCHAR(100) NOT NULL,
-                        FOREIGN KEY (MatchId) REFERENCES Match(Id)
+                        FOREIGN KEY (MatchId) REFERENCES Game(Id)
                         )
                     """
         self.cursor.execute(teamsStr)
         # create the Umpires table
         umpiresStr = """
-                    CREATE TABLE Umpires (
-                        MatchId INT ,
+                    CREATE TABLE Umpires(
+                        MatchId INT NOT NULL,
                         Umpire1 VARCHAR(100) NOT NULL,
                         Umpire2 VARCHAR(100) NOT NULL,
-                        FOREIGN KEY (MatchId) REFERENCES Match(Id)
+                        FOREIGN KEY (MatchId) REFERENCES Game(Id)
                         )
                     """
         self.cursor.execute(umpiresStr)
         # create the PlayerOfMatch table
         playerOfMatchStr = """
-                    CREATE TABLE PlayerOfMatch (
-                        MatchId INT ,
+                    CREATE TABLE PlayerOfMatch(
+                        MatchId INT NOT NULL,
                         Player VARCHAR(100) NOT NULL,
-                        FOREIGN KEY (MatchId) REFERENCES Match(Id)
+                        FOREIGN KEY (MatchId) REFERENCES Game(Id)
                         )
                     """
         self.cursor.execute(playerOfMatchStr)
         # create the Outcome table
         outComeStr = """
-                    CREATE TABLE Outcome (
-                        MatchId INT ,
+                    CREATE TABLE Outcome(
+                        MatchId INT NOT NULL,
                         Winner VARCHAR(100) NOT NULL,
                         Innings INT NULL,
                         Runs INT NULL,
                         Wickets INT NULL,
-                        FOREIGN KEY (MatchId) REFERENCES Match(Id)
+                        FOREIGN KEY (MatchId) REFERENCES Game(Id)
                         )
                     """
-        self.cursor.execute(playerOfMoutComeStratchStr)
+        self.cursor.execute(outComeStr)
         self.conn.commit()
+
+    def close(self):
+        """
+        Disconnect from DB
+        """
+        self.cursor.close()
+        self.conn.close()
