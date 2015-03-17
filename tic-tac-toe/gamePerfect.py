3759d55acf4bece0dbc9e32cc17c5566627906e6
bharatreddy <bharatr@vt.edu>
2015-03-17 17:45:34 +0530

    ttt-chwn

--- tic-tac-toe/gamePerfect.py
+++ tic-tac-toe/gamePerfect.py
@@ -12,3 +12,24 @@ class TicTacToe(object):
         # Also we'll use 'X', 'O' to represent the players
         # empty slots will be represented using #.
         self.board = ''.join( [ '#' for i in range(9) ] )
+
+    def check_winner(self):
+        # Here we check if there is winner in the board.
+        # Check the rows for winners.
+        for i in range(3):
+            if ( (self.board[i*3] == self.board[i*3+1]) and \
+                (self.board[i*3] ==  self.board[i*3+2]) and self.board[i] != '-' ):
+                return True
+        # Check the columns for winners.
+        for i in range(3):
+           if ( (self.board[i] == self.board[i+3]) and \
+                (self.board[i] ==  self.board[i+6]) and self.board[i] != '-' ):
+                return True
+        # Check the diagonal elements
+        # There are two possibilities elements 0, 4 and 8 or 2, 4 and 6.
+        if ( self.board[0] == self.board[4] and \
+            self.board[4] == self.board[8] and self.board[4] != '-' ):
+            return  True
+        if ( self.board[2] == self.board[4] and \
+            self.board[4] == self.board[6] and self.board[4] != '-' ):
+            return  True
