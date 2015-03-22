530d6b549e7f7a60233e760d3bd608d302efb3c0
bharatreddy <bharatr@vt.edu>
2015-03-22 14:28:30 +0530

    flask next move

--- tic-tac-toe/ticTac.py
+++ tic-tac-toe/ticTac.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+
 @app.route("/")
 def starter():
     return render_template('ttt-index.html')
+
+def next_move(board):
+    # given the current board retreive the next move.
+    # import the library
+    import gamePerfect
+    gmObj = gamePerfect.TicTacToe()
+    # set the board
+    gmObj.board = board
+    # return computer's step
+    step = gmObj.get_next_move('X')
+    return step[1]
 
 if __name__ == "__main__":
     app.debug=True
