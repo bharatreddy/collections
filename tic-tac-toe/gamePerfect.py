35f628cc97c8ffa8e0badf31d8037de43eb9b87e
bharatreddy <bharatr@vt.edu>
2015-03-23 12:15:41 +0530

    random in minimax

--- tic-tac-toe/gamePerfect.py
+++ tic-tac-toe/gamePerfect.py
@@ -134,3 +134,71 @@ class TicTacToe(object):
         else:
             minScore = min( resList )
             return ( minScore, emptySpots[ resList.index( minScore ) ] )
+
+
+    def get_next_move_dumb(self,currPlayer):
+        """
+            Function to calculate the next move using 
+            minimax algorithm. However, we make the  algo
+            a little dumb by making some moves random. We
+            start in a simple way by making 50 percent
+            of the moves random.
+            Note : This is a recursive algo and a knowledge of
+                the minimax algorithm is required to understand this.
+            Input(s) : currPlayer - current player ('X' or 'O')
+            Output(s) : (score, nextPos)
+                         (1) score --> +10 if 'X' is winning, 
+                           0 if draw and -10 if 'O' is winning
+                         (2) nextPos --> Position for next move.
+        """
+        # get a random number
+        import random
+        currRandom = random.uniform( 0., 1. )
+        # Verify the inputs before we begin
+        assert( currPlayer == 'X' or currPlayer == 'O' )            
+        # If this is the first step return the center position by
+        # default, otherwise the calculation takes a lot of time.
+        if len( set( self.board ) ) == 1:
+            return (0,4)
+        # set the next player
+        if currPlayer == 'X':
+            nextPlayer = 'O'
+        else:
+            nextPlayer = 'X'
+        # check for a winner in the current step
+        if self.check_winner():
+            if currPlayer == 'X':
+                return (-10,-1)
+            else:
+                return (10,-1)
+        # Check for a draw in the current step
+        if self.check_draw():
+            return (0,-1)
+        # have a list to keep track of the results.
+        resList = []
+        # store the location of empty spots in the board
+        emptySpots = []
+        for ix,brd in enumerate( self.board ):
+            if brd == '#':
+                emptySpots.append( ix )
+        # if the random number is less than .5
+        # we use minimax algo else we generate 
+        # a random move.
+        if currRandom <= 0.5: 
+            # Now here is the main minimax part.
+            for es in emptySpots:
+                self.board[es] = currPlayer
+                # Recurse and get a final state for the current move
+                tmpScore, tmpMove = self.get_next_move( nextPlayer )
+                resList.append( tmpScore )
+                self.board[es] = '#'
+            # Return the score and move depending on the currPlayer
+            if currPlayer == 'X':
+                maxScore = max( resList )
+                return ( maxScore, emptySpots[ resList.index( maxScore ) ] )
+            else:
+                minScore = min( resList )
+                return ( minScore, emptySpots[ resList.index( minScore ) ] )
+        else:
+            # return a random empty spot
+            return ( 0, emptySpots[random.randint(0,len(emptySpots)-1)] )
